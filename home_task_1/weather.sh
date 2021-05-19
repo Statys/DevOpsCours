c7caa5a23a8157539dc4f67685e185f38f97e83c
Statys <vsastatys@gmail.com>
2021-05-19 23:55:54 +0500

    home task 1 added

---
+++ home_task_1/weather.sh
@@ -0,0 +1,5 @@
+json_curr_condition=$(curl -s wttr.in/$1?format=j1 | jq '.current_condition[0]')
+echo $1
+humidity=$(echo $json_curr_condition | jq '.humidity|tonumber')
+temp=$(echo $json_curr_condition | jq '.temp_C|tonumber')
+echo humidity:$humidity% temp:$temp$'\xc2\xb0'C
